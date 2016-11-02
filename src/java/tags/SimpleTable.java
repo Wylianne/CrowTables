d92f64be1a13d22b167aca6e2f01eb2771c678cf
Wylianne <wyliannec@gmail.com>
2016-11-01 21:55:26 -0300

    Update SimpleTable.java

--- src/java/tags/SimpleTable.java
+++ src/java/tags/SimpleTable.java
@@ -5,8 +5,13 @@
  */
 package tags;
 
+import AcessoDados.AcessoDados;
+import com.mysql.jdbc.ResultSetMetaData;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
@@ -16,53 +21,108 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
  * @author Wylianne
  */
 public class SimpleTable extends SimpleTagSupport{
-    private String value;
+    private String list;
+    private String presentedOnTable;
+    String[] tbody;
+    String table_content="<tbody>";
+    String table_b ="<tr>";
+    String[][] t_body;
+    String id_table;
+    
+    public void setList(String list) {
+        this.list = list;
+    }
 
-    public void setValue(String value) {
-        this.value = value;
+    public void setPresentedOnTable(String presentedOnTable) {
+        this.presentedOnTable = presentedOnTable;
     }
       
     
     public void doTag() throws IOException, JspException{
-        String[] tbody;
+        
        
-        switch (value) {
-            case "5":
-                tbody = new String[2];
-                tbody[0] = "Nome";
-                tbody[1] = "Telefone";
-                break;
-            default:
-                tbody = new String[3];
-                tbody[0] = "Apelido";
-                tbody[1] = "CPF";
-                tbody[2] = "Email";    
+        switch (list) {
+            case "listaPessoas":
+                try {
+                        
+                        t_body = new String[8][2]; 
+                        t_body[0][0] = "id";
+                        t_body[0][1] = "ID";
+                        t_body[1][0] = "cpf";
+                        t_body[1][1] = "CPF";
+                        t_body[2][0] = "nome";
+                        t_body[2][1] = "Nome";
+                        t_body[3][0] = "identidade";
+                        t_body[3][1] = "Identidade";
+                        t_body[4][0] = "dataNascimento";
+                        t_body[4][1] = "Data de Nascimento";
+                        t_body[5][0] = "escolaridade";
+                        t_body[5][1] = "Escolaridade";
+                        t_body[6][0] = "endereco";
+                        t_body[6][1] = "Endereço";
+                        t_body[7][0] = "telefone";
+                        t_body[7][1] = "Telefone";
+                        
+                        ResultSet res;
+                        AcessoDados cliente = new AcessoDados();
+                        if (presentedOnTable!=null){
+                            //retorna somente o que esta presente em presentOnTable e id.
+                            String filter = presentedOnTable.replace(';', ',');
+                            res = cliente.Lista(filter);
+                            String tabela = list;
+                            filter = filter.replace(',', '-');
+                            id_table = tabela+filter;
+                        }else{
+                            res = cliente.Lista();
+                            String tabela = list;
+                            id_table = tabela;
+                        }
+                        
+                        
+                        //Verifica quantidade de colunas
+                        ResultSetMetaData rsmd = (ResultSetMetaData) res.getMetaData();  
+                        int numCol = rsmd.getColumnCount()+1; 
+                        
+                        //Nomes das colunas
+                        ResultSetMetaData titulos = (ResultSetMetaData) res.getMetaData();
+                        
+                        int q =0;
+                        while (res.next()) {
+                            //Monta conteudo
+                            table_content += "<tr>";
+                            for (int j = 1; j < numCol; j++) {
+                                table_content += "<td>"+res.getString(j)+"</td>"; 
+                            }
+                            table_content += "</tr>";                            
+                        }
+                        
+                        //Monta titulos
+                        
+                        for (int i = 1; i < numCol; i++) {
+                            for (String[] t_body1 : t_body) {
+                                if (titulos.getColumnName(i).equals(t_body1[0])) {
+                                    table_b += "<th>" + t_body1[1] +"</th>";
+                                }
+                            }
+                        }
 
+                        table_b = table_b+"</tr>";
+                        
+                } catch (Exception ex) {
+                    Logger.getLogger(SimpleContentTable.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            
    
         }
         
         
-        StringWriter write = new StringWriter();
-         
-        String table_b ="<tr>";
-       
-        for (int i = 0; i < tbody.length; i++) {
-            if(tbody[i] == ""){
-                break;
-            }
-            table_b += "<th>"+tbody[i]+"</th>";
-        }
-        
-        table_b = table_b+"</tr>";
-        
-        getJspBody().invoke(write);
-    
-        getJspContext().getOut().println("<table id='"+value+"' class='table table-striped table-bordered'>"
+        getJspContext().getOut().println("<table id='"+id_table+"' class='table table-striped table-bordered'>"
                 + "<thead>"+table_b+"</thead>"
-                + "<tfoot>"+table_b+"</tfoot>"+write.toString()+"</table>"
+                + "<tfoot>"+table_b+"</tfoot>"+table_content+"</table>"
                 + "<script>\n" 
                 + "            $(document).ready(function(){\n" 
-                + "                $('#"+value+"').DataTable();});\n" 
+                + "                $('#"+id_table+"').DataTable();});\n" 
                 + "</script>");
     }
     
